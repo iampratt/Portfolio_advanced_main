ade759aef2c033b3338316910ace82ec1033f33e
Pratyush Srivatava <pratyush0705@gmail.com>
2024-06-26 03:58:40 +0530

    fix: bg image

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import dotted from '../../public/assets/dotted_halftone.png';
 const home = () => {
   return (
     <>
       <div className='flex flex-col h-[calc(100vh-70px)] z-10'>
-        <div className='fixed bg-dotted h-[calc(65vh)] w-[calc(70vw)] bg-cover z-20 opacity-[0.2] bottom-0 left-[15vw]' />
+        <div className='fixed bg-[url("../../public/assets/dotted_halftone.png")] h-[calc(65vh)] w-[calc(70vw)] bg-cover z-20 opacity-[0.2] bottom-0 left-[15vw]' />
         <div className='p-3 w-[80vw]'>
           <div className='font-urbanist font-bold text-4xl sm:text-8xl'>
             Let's build something great together
