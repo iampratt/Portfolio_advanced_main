95a85a94f361a09913f3c49c1ae1d11b99800f70
Pratyush Srivatava <pratyush0705@gmail.com>
2024-06-26 18:36:41 +0530

    feat: about section

---
+++ src/assets/data.js
@@ -0,0 +1,21 @@
+const data = {
+  about: [
+    [
+      'Tech background',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consectetur repellendus pariatur sed. Ea exercitationem mollitia qui laborum, similique itaque voluptatum rerum atque, ducimus, vel quam unde officia at iure!',
+    ],
+    [
+      'AI & eyond',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consectetur repellendus pariatur sed. Ea exercitationem mollitia qui laborum, similique itaque voluptatum rerum atque, ducimus, vel quam unde officia at iure!',
+    ],
+    [
+      'Innovative',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consectetur repellendus pariatur sed. Ea exercitationem mollitia qui laborum, similique itaque voluptatum rerum atque, ducimus, vel quam unde officia at iure!',
+    ],
+    [
+      'Past experience',
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis voluptas quae ut dignissimos ipsa voluptates dolor reprehenderit, excepturi ex aspernatur labore, aliquam impedit, rem harum iste voluptatibus maxime! Itaque, quo.',
+    ],
+  ],
+};
+export default data;
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import data from '../assets/data';
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <div className='flex flex-col h-[calc(100vh-70px)] z-10 p-3'>
+      <div className='flex flex-row text-pale-gray font-urbanist text-2xl font-semibold justify-center'>
+        Hey there! I am Pratyush
+      </div>
+      <div className='mt-10 snap-x snap-mandatory flex flex-row overflow-scroll'>
+        {data.about.map((value) => {
+          console.log(value);
+          return (
+            <div className='snap-start pb-2'>
+              <div className='text-vivid-green text-nowrap mx-2 flex justify-center'>
+                {value[0]}
+              </div>
+              <div className='border-b-[3px] mt-2' />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default about;
